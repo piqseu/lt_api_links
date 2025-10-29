f06814378acbf7922388b8596038a73b796d6fa5
madoiscool <110845835+madoiscool@users.noreply.github.com>
2025-10-29 21:43:18 +1100

    Create [tag].js

---
+++ api/get-plugin/[tag].js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  const { tag } = req.query;
+  const url = `https://github.com/madoiscool/ltsteamplugin/releases/download/${tag}/ltsteamplugin.zip`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    res.status(response.status).send('Failed to fetch file.');
+    return;
+  }
+  res.setHeader('Content-Type', 'application/zip');
+  res.setHeader('Content-Disposition', `attachment; filename="ltsteamplugin-${tag}.zip"`);
+  const fileBuffer = await response.arrayBuffer();
+  res.send(Buffer.from(fileBuffer));
+}
